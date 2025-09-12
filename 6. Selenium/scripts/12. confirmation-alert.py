a09b92c9165fdfcccb0ec8bf5780aa91a71567fa
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2025-09-12 13:14:51 +0530

    alerts code done

---
+++ 6. Selenium/scripts/12. confirmation-alert.py
@@ -0,0 +1,43 @@
+# Import necessary libraries
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
+
+# Set up Chrome driver with undetected chromedriver to avoid bot detection
+# This code does not detect captchas
+options = uc.ChromeOptions()
+options.add_argument("--no-sandbox")  # Disable sandbox mode
+options.add_argument("--disable-blink-features=AutomationControlled")  # Disable automation detection
+driver = uc.Chrome(options=options, use_subprocess=True)  # Create driver instance
+driver.maximize_window()  # Maximize browser window for better visibility
+
+
+
+url = "https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm"
+driver.get(url)  # Navigate to the URL  
+time.sleep(1)  # Wait for the page to load
+
+
+iframe_element = driver.find_element(by= By.ID, value='iframeResult')
+driver.switch_to.frame(iframe_element)  # Switch to the iframe containing the alert button
+
+
+button = driver.find_element(by= By.XPATH, value= '/html/body/button')
+button.click()  # Click the button to trigger the alert
+
+print(f"Alert Text: {driver.switch_to.alert.text}")
+
+# driver.switch_to.alert.accept()  # Accept the alert
+# time.sleep(1)  # Wait for a few seconds to observe the result
+# print(f"\nYou clicked OK!")
+
+driver.switch_to.alert.dismiss()  # Dismiss the alert
+time.sleep(1)  # Wait for a few seconds to observe the result
+print("\nYou clicked Cancel!")
+
+
+driver.switch_to.default_content()  # Switch back to the main content
+
+driver.quit()  # Close the browser and end the session
